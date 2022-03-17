1884fb0bb2c824293b553ac126b6848a3bcc6d2a
Kyle Franz <kmfranz@protonmail.com>
2022-03-16 19:23:17 -0700

    add root key support to send native asset

--- scripts/send/send-native-asset.js
+++ scripts/send/send-native-asset.js
@@ -26,6 +26,11 @@ const options = yargs
         describe: "the file with the recovery string",
         required: true
     })
+    .options("k", {
+        alias: "root-key",
+        describe: "Root key (bech_32). Use this instead of the recovery file",
+        required: false,
+    })
     .option('p', {
         alias: 'policyid',
         describe: 'the policy id of the native token',
@@ -55,10 +60,16 @@ const options = yargs
 
 (async () => {
     try{
-        const data = fs.readFileSync(options['recovery-file'])
-        const entropyString = data.toString().trim()
-        const entropy = mnemonicToEntropy(entropyString)
-        const rootKey = createRootKeyFromEntropy(entropy)
+        let rootKey
+        if(options['recovery-file']){
+            const data = fs.readFileSync(options['recovery-file'])
+            const entropyString = data.toString().trim()
+            const entropy = mnemonicToEntropy(entropyString)
+            rootKey = createRootKeyFromEntropy(entropy)
+        } else {
+            rootKey = slib.Bip32PrivateKey.from_bech32(options['root-key'])
+        }
+
         const accountKey = createAccountKeyFromRootKey(rootKey)
         const privateKey = createPrivateKeyFromAccountKey(accountKey)
         const baseAddress = getSimpleBaseAddressForAccountKey(accountKey)
