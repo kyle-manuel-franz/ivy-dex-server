cc9b146289e81304ae1c3dcf2586c3e605b21514
Kyle Franz <kmfranz@protonmail.com>
2022-03-18 09:19:09 -0700

    add some cors support for local development

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ const express = require('express')
 const config = require('config')
 const port = config.port
 
+// TODO: turn this off in production
+const cors = require('cors');
+
 const tokenModel = require('./data/tokens/model')
 
 const { initializeMongoDbConnection } = require('./lib/mongoose')
 
 const app = express()
+app.use(cors())
 initializeMongoDbConnection().then()
 
 app.get('/api/tokens', async (req, res, next) => {
     const tokens = await tokenModel.find()
+    console.log('api/tokens')
     res.send(JSON.stringify(tokens))
 })
 
