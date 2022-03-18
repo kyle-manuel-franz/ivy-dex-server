8d1141c5b5f96f6c7af320a680d55ad7f44c7355
Kyle Franz <kmfranz@protonmail.com>
2022-03-18 09:22:21 -0700

    add standard logging to express app

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ const port = config.port
 
 // TODO: turn this off in production
 const cors = require('cors');
-
+const morgan = require('morgan')
 const tokenModel = require('./data/tokens/model')
 
 const { initializeMongoDbConnection } = require('./lib/mongoose')
 
 const app = express()
 app.use(cors())
+app.use(morgan('common'))
 initializeMongoDbConnection().then()
 
 app.get('/api/tokens', async (req, res, next) => {
