a2f95a6bf4b81ff0a1e425973e0550127e25b03b
Kyle Franz <kmfranz@protonmail.com>
2022-03-19 12:14:42 -0700

    add web handler to create transaction outputs with hashed plutus datum for place order

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const config = require('config')
 const port = config.port
-
+const slib = require('@emurgo/cardano-serialization-lib-nodejs')
 // TODO: turn this off in production
 const cors = require('cors');
-
+const { Buffer } = require('buffer')
 const morgan = require('morgan')
 const tokenModel = require('./data/tokens/model')
+const _ = require('lodash')
+
+const {
+    SCRIPT_ADDRESS
+} = require('../env/data/addresses')
 
 const { initializeMongoDbConnection } = require('./lib/mongoose')
+
+const { createOutputsForPlaceOrder } = require('./domain/transactions/commands')
 
 const app = express()
 app.use(cors())
@@ -20,16 +27,53 @@ app.get('/api/tokens', async (req, res, next) => {
     res.send(JSON.stringify(tokens))
 })
 
-app.get('/api/tx/place_order', async (req, res, next) => {
-    // build transaction for place order
-    // this should just build the bytes buffer for the datum and other peices
-    const ret_shape = {
-        order_datum: "",
-        outputs: "",
-        redeemers: ""
+// TODO: move this to a separate router and handler with validation and what not
+app.get('/api/tx_outputs/place_order', async (req, res, next) => {
+    const ownerAddressRaw = req.query.ownerAddress
+    const buyerAsset = req.query.buyerAsset
+    const sellerAsset = req.query.sellerAsset
+    const sellerAmount = req.query.sellerAmount
+    const buyerAmount = req.query.buyerAmount
+
+    let buyerCurrencySymbol, buyerTokenName
+    if(buyerAsset === ''){
+        buyerCurrencySymbol = ''
+        buyerTokenName = ''
+    } else {
+        buyerCurrencySymbol = buyerAsset.substring(0, 56)
+        buyerTokenName = buyerAsset.substring(56, buyerAsset.length)
     }
+
+    let sellerCurrencySymbol, sellerTokenName
+    if(sellerAsset === ''){
+        sellerCurrencySymbol = ''
+        sellerTokenName = ''
+    } else {
+        sellerCurrencySymbol = buyerAsset.substring(0, 56)
+        sellerTokenName = buyerAsset.substring(56, buyerAsset.length)
+    }
+
+    const ownerAddress = slib.Address.from_bytes(Buffer.from(ownerAddressRaw, 'hex'))
+    const ownerPubKeyHash = Buffer.from(slib.BaseAddress.from_address(ownerAddress).payment_cred().to_keyhash().to_bytes()).toString('hex')
+
+    const bookAddress = slib.Address.from_bech32(SCRIPT_ADDRESS)
+    const odBookPubKeyHash = Buffer.from(slib.BaseAddress.from_address(bookAddress).payment_cred().to_keyhash().to_bytes()).toString('hex')
+
+    const outputs = await createOutputsForPlaceOrder({
+        odOwner: ownerPubKeyHash,
+        odBook: odBookPubKeyHash,
+
+        odBuyerTokenName: buyerTokenName,
+        odBuyerCurrencySymbol: buyerCurrencySymbol,
+        odBuyerTokenAmount: buyerAmount,
+
+        odSellerTokenName: sellerTokenName,
+        odSellerCurrencySymbol: sellerCurrencySymbol,
+        odSellerTokenAmount: sellerAmount
+    }, SCRIPT_ADDRESS)
+
     res.status(200)
-    res.send('success')
+    res.send(_.map(outputs, o => Buffer.from(o).toString('hex')))
 })
 
 app.get('/api/tx/take_order', async (req, res, next) => {
